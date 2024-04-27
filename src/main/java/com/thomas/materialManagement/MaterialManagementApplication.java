db1e2ef391e0b7f853f54c1679d96331b8fdeea3
Thomas <1761174609@qq.com>
2024-04-28 00:46:47 +0800

    唐君恒 2024/4/28 00:46 项目代码初始化

---
+++ src/main/java/com/thomas/materialManagement/MaterialManagementApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.materialManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialManagementApplication.class, args);
+	}
+
+}

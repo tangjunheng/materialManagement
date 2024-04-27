82bc6032e4270c1814e409dc326cf19fe6a13a13
Thomas <1761174609@qq.com>
2024-04-28 00:46:47 +0800

    唐君恒 2024/4/28 00:46 项目代码初始化

---
+++ src/test/java/com/thomas/materialManagement/MaterialManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package com.thomas.materialManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MaterialManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
